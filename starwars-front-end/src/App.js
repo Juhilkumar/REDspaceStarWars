3c5b8873999ad11ee87ebb4782db9b5fa2907931
juhil7734 <zalavadiyajuhil99@gmail.com>
2021-11-08 18:48:38 -0400

    Added List of Characters with all the details

--- starwars-front-end/src/App.js
+++ starwars-front-end/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import People from "./Components/People";
+import CharacterInfo from "./Components/CharecterInfo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload app with.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={People} />
+          <Route exact path="/characterInfo/:id" component={CharacterInfo} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ starwars-front-end/src/Components/People.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
+import axios from "axios";
+import { CardGroup, CardImg, CardTitle, CardBody, Card, Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function People() {
+  let history = useHistory();
+  const [characters, setCharacters] = useState(null);
+  const [words, setWords] = useState('');
+  const url = "https://swapi.dev/api/people";
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const response = await axios.get(url);
+      let json = JSON.parse(JSON.stringify(response.data.results));
+      setCharacters(json);
+    }
+    fetchUserData();
+  }, []);
+
+  const textChangeHandler = (event) => {
+    setWords(event.target.value);
+  };
+
+  return (
+    <div>
+      <label className="m-3">Search : </label>
+      <input type="text" name="searchText" onChange={textChangeHandler}/>
+
+      {characters?.map((data, key) => {
+        return (
+          <div key={key}>
+            <CardGroup>
+              <Card>
+                <CardImg
+                  alt="Image is not available"
+                  src="https://starwarsblog.starwars.com/wp-content/uploads/2019/08/rise-of-skywalker-poster-tall.jpg"
+                  className="w-25 mt-3 img-thumbnail img-responsive center-block d-block mx-auto"
+                />
+                <CardTitle tag="h5">{data.name}</CardTitle>
+                <CardBody>
+                  <Button onClick={() => {
+                      history.push(`/characterInfo/${key+1}`)
+                  }}>Click Me</Button>
+                </CardBody>
+              </Card>
+            </CardGroup>
+          </div>
+        );
+      })}
+
+     
+      
+      {characters?.map((data, key) => {
+        return (
+          <div key={key}>
+            <CardGroup>
+              <Card>
+                <CardImg
+                  alt="Image is not available"
+                  src="https://starwarsblog.starwars.com/wp-content/uploads/2019/08/rise-of-skywalker-poster-tall.jpg"
+                  className="w-25 mt-3 img-thumbnail img-responsive center-block d-block mx-auto"
+                />
+                <CardTitle tag="h5">{data.name}</CardTitle>
+                <CardBody>
+                  <Button onClick={() => {
+                      history.push(`/characterInfo/${key+1}`)
+                  }}>Click Me</Button>
+                </CardBody>
+              </Card>
+            </CardGroup>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default People;
