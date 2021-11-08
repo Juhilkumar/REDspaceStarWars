34b9594489cafb6aa9ec55d81ca14b9d9055a65e
juhil7734 <zalavadiyajuhil99@gmail.com>
2021-11-08 19:12:26 -0400

    Filter Added

--- starwars-front-end/src/Components/People.js
+++ starwars-front-end/src/Components/People.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function People() {
   let history = useHistory();
   const [characters, setCharacters] = useState(null);
-  const [words, setWords] = useState('');
+  const [filteredData, setFilteredData] = useState('');
   const url = "https://swapi.dev/api/people";
 
   useEffect(() => {
@@ -20,7 +20,7 @@ function People() {
   }, []);
 
   const textChangeHandler = (event) => {
-    setWords(event.target.value);
+    setFilteredData(event.target.value);
   };
 
   return (
@@ -28,7 +28,14 @@ function People() {
       <label className="m-3">Search : </label>
       <input type="text" name="searchText" onChange={textChangeHandler}/>
 
-      {characters?.map((data, key) => {
+      {characters?.filter((data) => 
+      {
+        if(filteredData == "") {
+          return data
+        } else if(data.name.toLowerCase().includes(filteredData.toLowerCase())) {
+          return data
+        }
+      })?.map((data, key) => {
         return (
           <div key={key}>
             <CardGroup>
@@ -49,30 +56,7 @@ function People() {
           </div>
         );
       })}
-
-     
       
-      {characters?.map((data, key) => {
-        return (
-          <div key={key}>
-            <CardGroup>
-              <Card>
-                <CardImg
-                  alt="Image is not available"
-                  src="https://starwarsblog.starwars.com/wp-content/uploads/2019/08/rise-of-skywalker-poster-tall.jpg"
-                  className="w-25 mt-3 img-thumbnail img-responsive center-block d-block mx-auto"
-                />
-                <CardTitle tag="h5">{data.name}</CardTitle>
-                <CardBody>
-                  <Button onClick={() => {
-                      history.push(`/characterInfo/${key+1}`)
-                  }}>Click Me</Button>
-                </CardBody>
-              </Card>
-            </CardGroup>
-          </div>
-        );
-      })}
     </div>
   );
 }
