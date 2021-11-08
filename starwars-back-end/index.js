35756659a346c1b3931868abb411348e09543cf7
Juhil <zalavadiyajuhil99@gmail.com>
2021-11-08 10:06:09 -0400

    Initialize project

--- starwars-back-end/index.js
+++ starwars-back-end/index.js
@@ -1,1 +1,22 @@
-console.log("Welcome to the backend")
+"use strict";
+var express = require('express');
+var app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+var port = process.env.port || 5002
+app.listen(port, () => {
+    console.log('Server started on port', port)
+})
+
+app.use(bodyParser.json({ limit: "1000mb" }));
+
+function setUpRoutes() {
+    const routes = require("./routes");
+    routes.setup(app);
+}
+
+setUpRoutes();
+
+module.exports = app;
